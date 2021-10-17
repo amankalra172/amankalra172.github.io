44c5aef922c9b53579f3e0373547ac78ef4a2e31
amankalra172 <amankalra172@users.noreply.github.com>
2021-10-17 08:14:05 +0000

    deploy: 191ba6768866a2775f48dbeec55946d04800b24e

---
+++ b3ead0f8-3a59144ba08395af0caf.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkwww=self.webpackChunkwww||[]).push([[662],{3520:function(t,n,r){r.d(n,{A8q:function(){return o}});var e=r(8575);function o(t){return(0,e.w_)({tag:"svg",attr:{viewBox:"0 0 24 24",fill:"none",stroke:"currentColor",strokeWidth:"2",strokeLinecap:"round",strokeLinejoin:"round"},child:[{tag:"path",attr:{d:"M4 12v8a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2v-8"}},{tag:"polyline",attr:{points:"16 6 12 2 8 6"}},{tag:"line",attr:{x1:"12",y1:"2",x2:"12",y2:"15"}}]})(t)}}}]);
+//# sourceMappingURL=b3ead0f8-3a59144ba08395af0caf.js.map
