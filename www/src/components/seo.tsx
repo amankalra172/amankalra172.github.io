9663190db6da8e32de75c467d902647846ff6879
Aman Kalra <aman.kalra@st.ovgu.de>
2021-10-17 18:54:18 +0200

    commit

--- www/src/components/seo.tsx
+++ www/src/components/seo.tsx
@@ -14,10 +14,6 @@ type SEOProps = {
 }
 
 /* istanbul ignore next */
-const faviconSrc =
-  process.env.NODE_ENV === `production`
-    ? `/favicon.svg`
-    : `data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='0.9em' font-size='90'>🔥</text></svg>`
 
 export const SEO: React.FC<SEOProps> = ({
   title,
@@ -49,19 +45,19 @@ export const SEO: React.FC<SEOProps> = ({
       <meta property="og:description" content={seo.description} />
       <meta property="og:image" content={seo.image} />
       <meta property="og:type" content="website" />
-      <meta property="og:see_also" content="https://github.com/LekoArts" />
-      <meta property="og:see_also" content="https://www.behance.net/lekoarts" />
-      <meta property="og:see_also" content="https://dribbble.com/LekoArts" />
-      <meta property="og:see_also" content="https://youtube.de/LekoArtsDE" />
-      <meta property="og:see_also" content="https://twitter.com/lekoarts_de" />
+      <meta property="og:see_also" content="https://www.linkedin.com/in/amankalra172/" />
+      <meta property="og:see_also" content="https://twitter.com/amankalra172" />
+      <meta property="og:see_also" content="https://github.com/amankalra172" />
+      <meta property="og:see_also" content="https://medium.com/thestorii" />
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:title" content={seo.title} />
       <meta name="twitter:url" content={seo.url} />
       <meta name="twitter:description" content={seo.description} />
       <meta name="twitter:image" content={seo.image} />
       <meta name="twitter:creator" content={twitter} />
-      <meta name="creator" content="LekoArts" />
-      <link rel="icon" type="image/svg+xml" href={faviconSrc} />
+      <meta name="creator" content="AmanKalra" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
       <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
       <meta name="msapplication-TileColor" content="#0f172a" />
       {noIndex && <meta name="robots" content="noindex, nofollow" />}
